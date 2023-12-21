17242724203b555b083609335bdc79f36abbdadb
DatQuoc <datquoc3003@gmail.com>
2023-12-21 19:59:38 +0700

    feat: Add project setup dependancies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,39 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended", 
+    "plugin:prettier/recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "prettier"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs','vite.config.ts'],
   parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+     version: "detect",
+    },
+  },
+  parserOptions: { "project": ["./tsconfig.json"] },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
   },
 }
