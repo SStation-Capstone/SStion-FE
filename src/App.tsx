907566355654fac2d554c11a36bab1a4e63c90e6
DatQuoc <datquoc3003@gmail.com>
2023-12-21 20:56:49 +0700

    feat:Add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Button } from 'antd'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -24,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      <button>{'Test'} </button>
+      <Button type='primary'>Button</Button>
     </>
   )
 }
