014037f668555fa3d55770608aaa60e059d1f314
BToan12 <105841085+BToan12@users.noreply.github.com>
2024-03-18 23:24:01 +0700

    update manager

--- src/pages/manager/manager-list.container.tsx
+++ src/pages/manager/manager-list.container.tsx
@@ -1,13 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
-import { Button, Card, Popconfirm } from 'antd';
+import { Button, Card, Col, Form, Input, Pagination, Popconfirm, Row } from 'antd';
 import Table, { ColumnsType } from 'antd/es/table';
 import { useState } from 'react';
 
-import managerService from '@/api/services/managerService';
+import { useListManager } from '@/api/services/managerService';
 import { IconButton, Iconify } from '@/components/icon';
+import { CircleLoading } from '@/components/loading';
 
-import { ManagerStationEdit, RoleModalProps } from './manger.edit';
+import { ManagerEdit } from './manger.edit';
 
+import { InputType } from '#/api';
 import { Manager } from '#/entity';
 
 const DEFAULE_ROLE_VALUE: Manager = {
@@ -15,19 +16,29 @@ const DEFAULE_ROLE_VALUE: Manager = {
   userName: '',
   email: '',
   phoneNumber: '',
+  fullName: '',
 };
 export default function ManageStationManagerList() {
-  const [roleModalPros, setRoleModalProps] = useState<RoleModalProps>({
-    formValue: { ...DEFAULE_ROLE_VALUE },
-    title: 'New',
-    show: false,
-    onOk: () => {
-      setRoleModalProps((prev) => ({ ...prev, show: false }));
-    },
-    onCancel: () => {
-      setRoleModalProps((prev) => ({ ...prev, show: false }));
-    },
-  });
+  const [form] = Form.useForm();
+
+  const [listRelateParams, setListRelateParams] = useState<InputType>();
+  const [clickOne, setClickOne] = useState<Manager>();
+  const [showInfo, setShowInfo] = useState(false);
+  const { data, isLoading } = useListManager(listRelateParams);
+  if (isLoading) return <CircleLoading />;
+
+  const onOpenFormHandler = (record?: Manager) => {
+    if (record) {
+      setClickOne(record);
+    } else {
+      setClickOne(undefined);
+    }
+    setShowInfo(true);
+  };
+
+  const closeAndRefetchHandler = async () => {
+    setShowInfo(false);
+  };
   const columns: ColumnsType<Manager> = [
     {
       title: 'Id',
@@ -61,7 +72,7 @@ export default function ManageStationManagerList() {
       width: 100,
       render: (_, record) => (
         <div className="flex w-full justify-center text-gray">
-          <IconButton onClick={() => onEdit(record)}>
+          <IconButton onClick={() => onOpenFormHandler(record)}>
             <Iconify icon="solar:pen-bold-duotone" size={18} />
           </IconButton>
           <Popconfirm title="Delete the Role" okText="Yes" cancelText="No" placement="left">
@@ -73,42 +84,78 @@ export default function ManageStationManagerList() {
       ),
     },
   ];
-
-  const onCreate = () => {
-    setRoleModalProps((prev) => ({
-      ...prev,
-      show: true,
-      title: 'Create New',
-      formValue: {
-        ...prev.formValue,
-        ...DEFAULE_ROLE_VALUE,
-      },
-    }));
+  const resetHandler = () => {
+    form.resetFields();
   };
 
-  const onEdit = (formValue: Manager) => {
-    setRoleModalProps((prev) => ({
-      ...prev,
-      show: true,
-      title: 'Edit',
-      formValue,
-    }));
+  const onPageChange = (page: number, pageSize: number) => {
+    const values: InputType = { PageIndex: page, PageSize: pageSize };
+    setListRelateParams(values);
   };
-  const { data } = useQuery({
-    queryKey: ['manager'],
-    queryFn: managerService.GetManager,
-  });
-  console.log('data', data);
+  const onFinishHandler = (values: InputType) => {
+    setListRelateParams(values);
+  };
+
+  // const onCreate = () => {
+  //   setRoleModalProps((prev) => ({
+  //     ...prev,
+  //     show: true,
+  //     title: 'Create New',
+  //     formValue: {
+  //       ...prev.formValue,
+  //       ...DEFAULE_ROLE_VALUE,
+  //     },
+  //   }));
+  // };
+
+  // const onEdit = (formValue: Manager) => {
+  //   setRoleModalProps((prev) => ({
+  //     ...prev,
+  //     show: true,
+  //     title: 'Edit',
+  //     formValue,
+  //   }));
+  // };
+  // const { data } = useQuery({
+  //   queryKey: ['manager'],
+  //   queryFn: managerService.GetManager,
+  // });
+  // console.log('data', data);
 
   return (
     <Card
       title="Manager List"
       extra={
-        <Button type="primary" onClick={onCreate}>
+        <Button type="primary" onClick={() => onOpenFormHandler()}>
           New
         </Button>
       }
     >
+      <Form form={form} onFinish={onFinishHandler}>
+        <Row gutter={24} justify="space-between">
+          <Col span={8}>
+            <Form.Item name="Search">
+              <Input placeholder="Search by name" allowClear />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Row>
+              <Col span={12}>
+                <Form.Item name="search">
+                  <Button type="primary" htmlType="submit">
+                    Search
+                  </Button>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Button type="primary" onClick={resetHandler}>
+                  Reset
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Form>
       <Table
         rowKey="id"
         size="small"
@@ -116,9 +163,19 @@ export default function ManageStationManagerList() {
         pagination={false}
         columns={columns}
         dataSource={data?.contends}
+        loading={isLoading}
+      />
+      <Pagination
+        showSizeChanger
+        onChange={onPageChange}
+        total={data?.totalPages}
+        // showTotal={(total) => `共 ${total} 条`}
+        current={data?.page}
+        style={{ marginTop: '1rem' }}
       />
 
-      <ManagerStationEdit {...roleModalPros} />
+      {/* <ManagerEdit {...roleModalPros} /> */}
+      {showInfo && <ManagerEdit clickOne={clickOne} onClose={closeAndRefetchHandler} />}
     </Card>
   );
 }
