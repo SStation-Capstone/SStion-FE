3f6f1a1f47205fd45ae1010bbd7811a1f2d1f34e
Quoc Dat <83881243+QuocDatFPTU@users.noreply.github.com>
2024-04-13 03:59:30 +0700

    feat: noti feat (#33)

--- src/layouts/_common/notice.tsx
+++ src/layouts/_common/notice.tsx
@@ -1,18 +1,24 @@
-import { faker } from '@faker-js/faker';
-import { Badge, Button, Drawer, Space, Tabs, TabsProps } from 'antd';
+import { Badge, Drawer, Tabs, TabsProps, Tag } from 'antd';
 import Color from 'color';
 import { CSSProperties, ReactNode, useState } from 'react';
 
+import { useListNoti, useReadAllNoti } from '@/api/services/notiService';
 import CyanBlur from '@/assets/images/background/cyan-blur.png';
 import RedBlur from '@/assets/images/background/red-blur.png';
-import { IconButton, Iconify, SvgIcon } from '@/components/icon';
+import { IconButton, Iconify } from '@/components/icon';
+import { CircleLoading } from '@/components/loading';
 import ProTag from '@/theme/antd/components/tag';
 import { useThemeToken } from '@/theme/hooks';
 
 export default function NoticeButton() {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const themeToken = useThemeToken();
-  const [count, setCount] = useState(4);
+
+  const [selectedLevel, setSelectedLevel] = useState<string>('');
+  const { data } = useListNoti({ Level: selectedLevel });
+  const { data: dataFetch, isLoading } = useListNoti({ Level: '' });
+  const { mutateAsync: readAllMutate } = useReadAllNoti();
+  if (isLoading) return <CircleLoading />;
 
   const style: CSSProperties = {
     backdropFilter: 'blur(20px)',
@@ -26,11 +32,91 @@ export default function NoticeButton() {
     padding: 0,
   };
 
+  const tabChildren: ReactNode = data?.contends.map((e) => {
+    return (
+      <div className="mb-3 cursor-pointer text-sm" key={e.id}>
+        <div className="flex">
+          {e.level === 'Information' && (
+            <Tag className="self-center" color="cyan">
+              Info
+            </Tag>
+          )}
+          {e.level === 'Warn' && <Tag color="orange">Warn</Tag>}
+          {e.level === 'Critical' && <Tag color="red">Critical</Tag>}
+          <div className="ml-2">
+            <div>
+              <span className="font-medium">{e.createdBy} </span>
+              <span className="text-xs font-light">{e.content}</span>
+            </div>
+            <span className="text-xs font-light opacity-60">
+              at{' '}
+              {new Date(e.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}{' '}
+              {new Date(e.createdAt).toISOString().slice(0, 10)}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  const items: TabsProps['items'] = [
+    {
+      key: 'All',
+      label: (
+        <div className="flex">
+          <span>All</span>
+          <ProTag color="processing"> {dataFetch?.contends.length}</ProTag>
+        </div>
+      ),
+      children: tabChildren,
+    },
+    {
+      key: 'Critical',
+      label: (
+        <div className="flex">
+          <span>Critical</span>
+          <ProTag color="error">
+            {data?.contends.filter((e) => e.level === 'Critical').length}
+          </ProTag>
+        </div>
+      ),
+      children: tabChildren,
+    },
+    {
+      key: 'Warning',
+      label: (
+        <div className="flex">
+          <span>Warning</span>
+          <ProTag color="warning">
+            {data?.contends.filter((e) => e.level === 'Warning').length}
+          </ProTag>
+        </div>
+      ),
+      children: tabChildren,
+    },
+    {
+      key: 'Information',
+      label: (
+        <div className="flex">
+          <span>Information</span>
+          <ProTag color="green">
+            {data?.contends.filter((e) => e.level === 'Information').length}
+          </ProTag>
+        </div>
+      ),
+      children: tabChildren,
+    },
+  ];
+
   return (
     <div>
-      <IconButton onClick={() => setDrawerOpen(true)}>
+      <IconButton
+        onClick={() => {
+          setDrawerOpen(true);
+        }}
+      >
         <Badge
-          count={count}
+          count={dataFetch?.countUnread}
           styles={{
             root: { color: 'inherit' },
             indicator: { color: '#fff' },
@@ -52,8 +138,8 @@ export default function NoticeButton() {
         extra={
           <IconButton
             style={{ color: themeToken.colorPrimary }}
-            onClick={() => {
-              setCount(0);
+            onClick={async () => {
+              await readAllMutate();
               setDrawerOpen(false);
             }}
           >
@@ -69,201 +155,20 @@ export default function NoticeButton() {
           </div>
         }
       >
-        <NoticeTab />
+        <div className="flex flex-col px-6">
+          <Tabs
+            defaultActiveKey="All"
+            items={items}
+            onChange={(key: string) => {
+              if (key === 'All') {
+                setSelectedLevel('');
+              } else {
+                setSelectedLevel(key);
+              }
+            }}
+          />
+        </div>
       </Drawer>
-    </div>
-  );
-}
-
-function NoticeTab() {
-  const themeToken = useThemeToken();
-  const tabChildren: ReactNode = (
-    <div className="text-sm">
-      <div className="flex">
-        <img className="h-10 w-10 rounded-full" src={faker.image.avatar()} alt="" />
-        <div className="ml-2">
-          <div>
-            <span className="font-medium">{faker.person.fullName()}</span>
-            <span className="text-xs font-light"> sent you a frind request</span>
-          </div>
-          <span className="text-xs font-light opacity-60">about 1 hour ago</span>
-          <div className="mt-2">
-            <Space>
-              <Button type="primary">Accept</Button>
-              <Button>Refuse</Button>
-            </Space>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <img className="h-10 w-10 rounded-full" src={faker.image.avatar()} alt="" />
-        <div className="ml-2">
-          <div>
-            <span className="font-medium">{faker.person.fullName()}</span>
-            <span className="text-xs font-light"> added file to </span>
-            <span className="font-medium">File Manager</span>
-          </div>
-          <span className="text-xs font-light opacity-60">5 hour ago</span>
-          <div
-            style={{ background: themeToken.colorBgContainerDisabled }}
-            className="mt-2 flex items-center rounded-lg bg-gray-200 p-4"
-          >
-            <div className="ml-2 flex flex-col text-gray">
-              <span className="font-medium">@{faker.person.fullName()}</span>
-              <span className="text-xs">{faker.lorem.lines(2)}</span>
-            </div>
-          </div>
-          <div className="mt-2">
-            <Space>
-              <Button type="primary">Reply</Button>
-            </Space>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <img className="h-10 w-10 rounded-full" src={faker.image.avatar()} alt="" />
-        <div className="ml-2">
-          <div>
-            <span className="font-medium">{faker.person.fullName()}</span>
-            <span className="text-xs font-light"> mentioned you in</span>
-            <span className="font-medium">Slash Admin</span>
-          </div>
-          <span className="text-xs font-light opacity-60">1 days ago</span>
-          <div className="mt-2">
-            <Space>
-              <Button type="primary">Reply</Button>
-            </Space>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <img className="h-10 w-10 rounded-full" src={faker.image.avatar()} alt="" />
-        <div className="ml-2">
-          <div>
-            <span className="font-medium">{faker.person.fullName()}</span>
-            <span className="text-xs font-light"> added file to </span>
-            <span className="font-medium">File Manager</span>
-          </div>
-          <span className="text-xs font-light opacity-60">2 days ago</span>
-          <div
-            style={{ background: themeToken.colorBgContainerDisabled }}
-            className="mt-2 flex items-center rounded-lg bg-gray-200 p-4"
-          >
-            <SvgIcon icon="ic_file_audio" size={48} />
-            <div className="ml-2 flex flex-col text-gray">
-              <span className="font-medium">Witout Me</span>
-              <span className="text-xs">1.2GB·30 min ago </span>
-            </div>
-            <Button className="ml-4">Download</Button>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <img className="h-10 w-10 rounded-full" src={faker.image.avatar()} alt="" />
-        <div className="ml-2">
-          <div>
-            <span className="font-medium">{faker.person.fullName()}</span>
-            <span className="text-xs font-light"> request a payment of </span>
-            <span className="font-medium">$3000</span>
-          </div>
-          <span className="text-xs font-light opacity-60">4 days ago</span>
-          <div className="mt-2">
-            <Space>
-              <Button type="primary">Pay</Button>
-              <Button>Refuse</Button>
-            </Space>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <IconButton>
-          <SvgIcon icon="ic_order" size={30} />
-        </IconButton>
-        <div className="ml-2">
-          <div>
-            <span className="font-light">Your order is placed waiting for shipping</span>
-          </div>
-          <span className="text-xs font-light opacity-60">4 days ago</span>{' '}
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <IconButton>
-          <SvgIcon icon="ic_mail" size={30} />
-        </IconButton>
-        <div className="ml-2">
-          <div>
-            <span className="font-light">You have new mail</span>
-          </div>
-          <span className="text-xs font-light opacity-60">5 days ago</span>{' '}
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <IconButton>
-          <SvgIcon icon="ic_chat" size={30} />
-        </IconButton>
-        <div className="ml-2">
-          <div>
-            <span className="font-light">You have new message 5 unread message</span>
-          </div>
-          <span className="text-xs font-light opacity-60">7 days ago</span>
-        </div>
-      </div>
-
-      <div className="mt-8 flex">
-        <IconButton>
-          <SvgIcon icon="ic_delivery" size={30} />
-        </IconButton>
-        <div className="ml-2">
-          <div>
-            <span className="font-light">Delivery processing your order is being shipped</span>
-          </div>
-          <span className="text-xs font-light opacity-60">8 days ago</span>{' '}
-        </div>
-      </div>
-    </div>
-  );
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <div className="flex">
-          <span>All</span>
-          <ProTag color="processing">22</ProTag>
-        </div>
-      ),
-      children: tabChildren,
-    },
-    {
-      key: '2',
-      label: (
-        <div className="flex">
-          <span>Unread</span>
-          <ProTag color="error">12</ProTag>
-        </div>
-      ),
-      children: tabChildren,
-    },
-    {
-      key: '3',
-      label: (
-        <div className="flex">
-          <span>Archived</span>
-          <ProTag color="green">10</ProTag>
-        </div>
-      ),
-      children: tabChildren,
-    },
-  ];
-  return (
-    <div className="flex flex-col px-6">
-      <Tabs defaultActiveKey="1" items={items} />
     </div>
   );
 }
